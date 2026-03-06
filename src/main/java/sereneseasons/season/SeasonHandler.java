b9bb69ab41c38aec38ca0ea65ccb98b5be2ef18e
joe-vettek <my1233215@126.com>
2026-03-06 13:02:33 +0800

    Create SeasonHandler.java

---
+++ src/main/java/sereneseasons/season/SeasonHandler.java
@@ -0,0 +1,13 @@
+package sereneseasons.season;
+
+import net.minecraft.world.level.Level;
+
+import java.util.HashMap;
+
+// required by AllTheLeaks
+@Deprecated(forRemoval = true)
+public class SeasonHandler {
+    public static final HashMap<Level, Long> lastDayTimes = new HashMap();
+    public static final HashMap<Level, Integer> updateTicks = new HashMap();
+    //public static final HashMap<ResourceKey<Level>, Integer> prevServerSeasonCycleTicks = new HashMap();
+}
