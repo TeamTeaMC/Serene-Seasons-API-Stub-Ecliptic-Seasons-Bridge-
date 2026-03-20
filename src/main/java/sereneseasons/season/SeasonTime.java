0f702a7a3df453d04d350030934e56d975c35517
joe-vettek <my1233215@126.com>
2026-03-20 21:45:40 +0800

    9.1.0.2-patch8-1

--- src/main/java/sereneseasons/season/SeasonTime.java
+++ src/main/java/sereneseasons/season/SeasonTime.java
@@ -1,5 +1,8 @@
 package sereneseasons.season;
 
+import com.teamtea.eclipticseasons_serene_compatibility.api.EclipticSeasonTime;
+import net.minecraft.world.level.Level;
+
 public final class SeasonTime extends EclipticSeasonTime {
     public static final SeasonTime ZERO = new SeasonTime(null);
 
