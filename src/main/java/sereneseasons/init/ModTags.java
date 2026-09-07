1c38c62ec034a4ccc0f54218411a0ad6e27ad7f2
joe-vettek <my1233215@126.com>
2026-09-07 13:21:27 +0800

    21.11.0.4-patch4-2

--- src/main/java/sereneseasons/init/ModTags.java
+++ src/main/java/sereneseasons/init/ModTags.java
@@ -3,12 +3,17 @@ package sereneseasons.init;
 import com.teamtea.eclipticseasons_serene_compatibility.SereneCompatibility;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.block.Block;
 
 @SuppressWarnings("removal")
 public class ModTags {
 
+    public static class Blocks {
+        public static final TagKey<Block> GREENHOUSE_GLASS = BlockTags.GRASS_BLOCKS;
+    }
 
     public static class Biomes {
         private static TagKey<Biome> tag(Identifier name) {
